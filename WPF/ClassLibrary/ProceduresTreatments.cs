80fde765f0ff3d72f2d591ad9004c5281b8db723
NadeemAbu <33332702+NadeemAbu@users.noreply.github.com>
2018-06-29 14:53:44 +1000

    Passing Task 4

---
+++ WPF/ClassLibrary/ProceduresTreatments.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class ProceduresTreatments
+    {
+        private string petName;
+        private DateTime date;
+        private string notes;
+        private decimal price;
+        private int ownerID;
+        private int procedureID;
+
+        public string PetName
+        {
+            get { return petName; }
+            set { petName = value; }
+        }
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        public string Notes
+        {
+            get { return notes; }
+            set { notes = value; }
+        }
+
+
+        public decimal Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public int OwnerID
+        {
+            get { return ownerID; }
+            set { ownerID = value; }
+        }
+
+        public int ProcedureID
+        {
+            get { return procedureID; }
+            set { procedureID = value; }
+        }
+
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+    }
+}
